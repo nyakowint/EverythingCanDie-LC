01c66f4b7428d06449630ccbcf1603373fa6f8a8
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-06-04 10:49:10 +0200

    Update Patches.cs
    removed the Lethal Things dependency (and consequently its configs) and updated the shotgun code.

--- EverythingCanDie/Patches.cs
+++ EverythingCanDie/Patches.cs
@@ -90,13 +90,6 @@ namespace EverythingCanDie
                                                 true, // The default value
                                                 "The value of whether this mob is shootable with things like shotguns.(WARNING: Only works for items that use the ShotgunItem as its parent)"); // Description
                     }
-                    if (!Plugin.Instance.Config.ContainsKey(new ConfigDefinition("Mobs", mobName + ".Rocketable")))
-                    {
-                        ConfigEntry<bool> tempEntry = Plugin.Instance.Config.Bind("Mobs", // The section under which the option is shown
-                                                mobName + ".Rocketable", // The key of the configuration option in the configuration file
-                                                true, // The default value
-                                                "The value of whether this mob is able to be shot with rockets from LethalThings rocket launcher(optional if LethalThings is installed)."); // Description
-                    }
                     if (!Plugin.Instance.Config.ContainsKey(new ConfigDefinition("Mobs", mobName + ".Health")))
                     {
                         EnemyAI enemyAI = enemy.enemyPrefab.GetComponent<EnemyAI>();
@@ -164,35 +157,13 @@ namespace EverythingCanDie
                         GrabbableObject held = playerWhoHit.ItemSlots[playerWhoHit.currentItemSlot];
                         if (held.itemProperties.isDefensiveWeapon && !Plugin.Can(name + ".Hittable"))
                         {
-                            if (!(held is ShotgunItem))
-                            {
-                                if (Plugin.hasEvaisaRPG)
-                                {
-                                    if (!held.GetType().IsEquivalentTo(Plugin.FindType("LethalThings.RocketLauncher")))
-                                    {
-                                        canDamage = false;
-                                        Plugin.Log.LogInfo($"Hit Disabled for {__instance.enemyType.enemyName}!");
-                                    }
-                                }
-                                else
-                                {
-                                    canDamage = false;
-                                    Plugin.Log.LogInfo($"Hit Disabled for {__instance.enemyType.enemyName}!");
-                                }
-                            }
+                            canDamage = false;
+                            Plugin.Log.LogInfo($"Hit Disabled for {__instance.enemyType.enemyName}!");
                         }
                         else if ((held is ShotgunItem) && !Plugin.Can(name + ".Shootable"))
                         {
                             canDamage = false;
                             Plugin.Log.LogInfo($"Shoot Disabled for {__instance.enemyType.enemyName}!");
-                        }
-                        else if (Plugin.hasEvaisaRPG && !Plugin.Can(name + ".Rocketable"))
-                        {
-                            if (held.GetType().IsEquivalentTo(Plugin.FindType("LethalThings.RocketLauncher")))
-                            {
-                                canDamage = false;
-                                Plugin.Log.LogInfo($"Rockets Disabled for {__instance.enemyType.enemyName}!");
-                            }
                         }
                     }
                     if (canDamage)
@@ -282,11 +253,6 @@ namespace EverythingCanDie
                         {
                             Object.Instantiate(Plugin.explosionPrefab, __instance.transform.position, Quaternion.Euler(-90f, 0f, 0f),
                             RoundManager.Instance.mapPropsContainer.transform).SetActive(value: true);
-
-                            if (!__instance.isEnemyDead)
-                            {
-                                __instance.KillEnemyOnOwnerClient(false);
-                            }
                         }
                     }
                     else
@@ -296,23 +262,8 @@ namespace EverythingCanDie
                         {
                             Object.Instantiate(Plugin.explosionPrefab, __instance.transform.position, Quaternion.Euler(-90f, 0f, 0f),
                             RoundManager.Instance.mapPropsContainer.transform).SetActive(value: true);
-                            if (!__instance.isEnemyDead)
-                            {
-                                __instance.KillEnemyOnOwnerClient(false);
-                            }
                         }
                         __instance.StartCoroutine(MoveBody(__instance, 0.1f));
-                    }
-                }
-                else
-                {
-                    if (UnkillableEnemies.Contains(type.enemyName))
-                    {
-                        if (!__instance.isEnemyDead && __instance.IsServer)
-                        {
-                            __instance.KillEnemyOnOwnerClient(false);
-                        }
-                        __instance.StartCoroutine(MoveBody(__instance, 4));
                     }
                 }
             }
@@ -344,16 +295,15 @@ namespace EverythingCanDie
             if (playerFired)
             {
                 // correct offset to something more reasonable when a player fires
-                //shotgunPosition += GameNetworkManager.Instance.localPlayerController.gameplayCamera.transform.up * 0.25f; // vanilla code is -0.45
-                shotgunPosition = gun.shotgunRayPoint.position;
+                shotgunPosition += GameNetworkManager.Instance.localPlayerController.gameplayCamera.transform.up * 0.25f; // vanilla code is -0.45
             }
             bool thisPlayerFired = playerFired && gun.playerHeldBy == GameNetworkManager.Instance.localPlayerController;
             if (thisPlayerFired) gun.playerHeldBy.playerBodyAnimator.SetTrigger("ShootShotgun");
 
             // fire and reduce shell count - copied from vanilla
 
-            RoundManager.PlayRandomClip(gun.gunShootAudio, gun.gunShootSFX, randomize: true, 1f, 1840);
-            WalkieTalkie.TransmitOneShotAudio(gun.gunShootAudio, gun.gunShootSFX[0]);
+            RoundManager.PlayRandomClip(gun.GetComponent<ShotgunItem>().gunShootAudio, gun.GetComponent<ShotgunItem>().gunShootSFX, randomize: true, 1f, 1840);
+            WalkieTalkie.TransmitOneShotAudio(gun.GetComponent<ShotgunItem>().gunShootAudio, gun.GetComponent<ShotgunItem>().gunShootSFX[0]);
             gun.gunShootParticle.Play(withChildren: true);
 
             gun.isReloading = false;
@@ -367,7 +317,6 @@ namespace EverythingCanDie
             // generate pellet vectors (done separately to minimise time random state is modified)
             var vectorList = new Vector3[Plugin.numTightPellets + Plugin.numLoosePellets];
             var oldRandomState = UnityEngine.Random.state;
-            UnityEngine.Random.InitState(UnityEngine.Random.Range(0, int.MaxValue));
             for (int i = 0; i < Plugin.numTightPellets + Plugin.numLoosePellets; i++)
             {
                 float variance = (i < Plugin.numTightPellets) ? Plugin.tightPelletAngle : Plugin.loosePelletAngle;
@@ -397,9 +346,9 @@ namespace EverythingCanDie
             if (earRingSeverity > 0f && SoundManager.Instance.timeSinceEarsStartedRinging > 16f && !playerFired)
                 gun.StartCoroutine(DelayedEarsRinging(earRingSeverity));
 
-            List<GameObject> targets = new List<GameObject>();
             // raycast those vectors to find hits
             Ray ray;
+            var counts = new Plugin.CountHandler();
             // TODO: modify count tracker to handle distance pellets travel? sqrt(1-dist/range) seems reasonable for damage worth
             for (int i = 0; i < vectorList.Length; i++)
             {
@@ -408,31 +357,26 @@ namespace EverythingCanDie
                 RaycastHit[] hits = Physics.RaycastAll(ray, Plugin.range, playerFired ? Plugin.PLAYER_HIT_MASK : Plugin.ENEMY_HIT_MASK, QueryTriggerInteraction.Collide);
                 Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
                 Vector3 end = shotgunPosition + vect * Plugin.range;
+                Debug.Log("SHOTGUN: RaycastAll hit " + hits.Length + " things (" + playerFired + "," + thisPlayerFired + ")");
                 for (int j = 0; j < hits.Length; j++)
                 {
                     GameObject obj = hits[j].transform.gameObject;
                     if (obj.TryGetComponent(out IHittable hittable))
                     {
-                        if (ReferenceEquals(hittable, gun.playerHeldBy))
-                            continue; // self hit
+                        if (ReferenceEquals(hittable, gun.playerHeldBy)) continue; // self hit
                         EnemyAI ai = null;
-                        if (hittable is EnemyAICollisionDetect detect)
-                            ai = detect.mainScript;
+                        if (hittable is EnemyAICollisionDetect detect) ai = detect.mainScript;
                         if (ai != null)
                         {
-                            if (!playerFired)
-                                continue; // enemy hit enemy
-                            if (ai.isEnemyDead || ai.enemyHP <= 0 || !ai.enemyType.canDie)
-                                continue; // skip dead things
+                            if (!playerFired) continue; // enemy hit enemy
+                            if (ai.isEnemyDead || ai.enemyHP <= 0 || !ai.enemyType.canDie) continue; // skip dead things
                         }
-                        if (hittable is PlayerControllerB)
-                            targets.Add(obj);
-                        else if (ai != null)
-                            targets.Add(obj);
-                        else if (playerFired)
-                            targets.Add(obj);
+                        if (hittable is PlayerControllerB) counts.AddPlayerToCount(hittable as PlayerControllerB);
+                        else if (ai != null) counts.AddEnemyToCount(ai);
+                        else if (playerFired) counts.AddOtherToCount(hittable);
                         else continue; // enemy hit something else (webs?)
                         end = hits[j].point;
+                        Debug.Log("SHOTGUN: Hit [" + hittable + "] (" + (j + 1) + "@" + Vector3.Distance(shotgunPosition, end) + ")");
                         break;
                     }
                     else
@@ -440,62 +384,41 @@ namespace EverythingCanDie
                         // precaution: hit enemy without hitting hittable (immune to shovels?)
                         if (hits[j].collider.TryGetComponent(out EnemyAI ai))
                         {
-                            if (playerFired && !ai.isEnemyDead && ai.enemyHP > 0)
+                            if (playerFired && !ai.isEnemyDead && ai.enemyHP > 0 && ai.enemyType.canDie)
                             {
-                                targets.Add(ai.gameObject);
+                                counts.AddEnemyToCount(ai);
                                 end = hits[j].point;
+                                Debug.Log("SHOTGUN: Backup hit [" + ai + "] (" + (j + 1) + "@" + Vector3.Distance(shotgunPosition, end) + ")");
                                 break;
                             }
                             else continue;
                         }
                         end = hits[j].point;
+                        Debug.Log("SHOTGUN: Wall [" + obj + "] (" + (j + 1) + "@" + Vector3.Distance(shotgunPosition, end) + ")");
                         break; // wall or other obstruction
                     }
                 }
                 Plugin.VisualiseShot(shotgunPosition, end);
             }
+
             // deal damage all at once - prevents piercing alive and reduces damage calls
-            targets.RemoveAll(t => t == null);
-            if (targets.Any())
-            {
-                targets.ForEach(t =>
-                {
-                    if (t == null)
-                    {
-                        Plugin.Log.LogInfo("NO!");
-                    }
-                    else
-                    {
-                        if (t.GetComponent<EnemyAI>() != null)
-                        {
-                            EnemyAI enemy = t.GetComponent<EnemyAI>();
-                            int damage = 1;
-                            enemy.HitEnemyOnLocalClient(damage, default, holder);
-                        }
-                        else if (t.GetComponent<IHittable>() != null)
-                        {
-                            IHittable hit = t.GetComponent<IHittable>();
-                            if (hit is EnemyAICollisionDetect)
-                            {
-                                EnemyAICollisionDetect enemy = (EnemyAICollisionDetect)hit;
-                                int damage = 1;
-                                enemy.mainScript.HitEnemyOnLocalClient(damage, default, holder);
-                            }
-                            else if (hit is PlayerControllerB)
-                            {
-                                PlayerControllerB player = (PlayerControllerB)hit;
-                                // grouping player damage also ensures strong hits (3+ pellets) ignore critical damage - 5 is always lethal rather than being critical
-                                int damage = 18;
-                                player.DamagePlayer(damage, true, true, CauseOfDeath.Gunshots, 0, false, shotgunForward);
-                            }
-                            else
-                            {
-                                hit.Hit(1, shotgunForward, gun.playerHeldBy, true);
-                            }
-                        }
-                    }
-                });
-            }
+            counts.player.ForEach(p => {
+                // grouping player damage also ensures strong hits (3+ pellets) ignore critical damage - 5 is always lethal rather than being critical
+                int damage = p.count * 20;
+                Debug.Log("SHOTGUN: Hit " + p.item + " with " + p.count + " pellets for " + damage + " damage");
+                p.item.DamagePlayer(damage, true, true, CauseOfDeath.Gunshots, 0, false, shotgunForward);
+            });
+            counts.enemy.ForEach(e => {
+                // doing 1:1 damage is too strong, but one pellet should always do damage
+                int damage = e.count / 2 + 1; // half rounded down plus one (1,2,2,3,3,4,4,5,5,6)
+                Debug.Log("SHOTGUN: Hit " + e.item + " with " + e.count + " pellets for " + damage + " damage");
+                e.item.HitEnemyOnLocalClient(damage, shotgunForward, gun.playerHeldBy, true);
+            });
+            counts.other.ForEach(o => {
+                int damage = o.count / 2 + 1;
+                Debug.Log("SHOTGUN: Hit " + o.item + " with " + o.count + " pellets for " + damage + " damage");
+                o.item.Hit(damage, shotgunForward, gun.playerHeldBy, true);
+            });
 
             ray = new Ray(shotgunPosition, shotgunForward);
             if (Physics.Raycast(ray, out RaycastHit hitInfo, Plugin.range, StartOfRound.Instance.collidersAndRoomMaskAndDefault, QueryTriggerInteraction.Ignore))
